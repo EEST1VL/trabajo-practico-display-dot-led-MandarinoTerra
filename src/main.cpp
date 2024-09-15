36e94174a0380ddf81991169608cf6d5c0cac949
MandarinoTerra <benjaminjunco383@gmail.com>
2024-09-15 05:15:21 -0300

    Mando el vector a los leds check

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,19 @@
 
 #define set_bit(reg, bit) reg |= (1 << bit)
 #define clear_bit(reg, bit) reg &= ~(1 << bit)
+uint64_t beats;
+uint64_t beats_ini;
+enum Matrix
+{
+    fila1,
+    fila2,
+    fila3,
+    fila4,
+    fila5,
+    fila6,
+    fila7
+};
+enum Matrix matris = fila4;
 uint8_t mux_vec[8] = {
     0b11111111,
     0b01000000,
@@ -17,47 +30,46 @@ uint8_t mux_vec[8] = {
 };
 uint8_t hola[41] =
     {
-        0b11111111,
         0b00000000,
-        0b00100100,
-        0b00100100,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00000000,
+        0b00101110,
+        0b00101010,
+        0b00111010,
+        0b00000000,
+        0b00000010,
+        0b00000010,
+        0b00111110,
+        0b00000000,
+        0b00100010,
+        0b00101010,
+        0b00111110,
+        0b00000000,
         0b00111100,
-        0b00100100,
-        0b00100100,
-        0b00000000,
-
-        0b00000000,
-        0b00100100,
-        0b00100100,
+        0b00000010,
+        0b0000100,
+        0b00000010,
         0b00111100,
-        0b00100100,
-        0b00100100,
-        0b00000000,
-        
-        0b00000000,
-        0b00100100,
-        0b00100100,
-        0b00111100,
-        0b00100100,
-        0b00100100,
-        0b00000000,
-        
-        0b00000000,
-        0b00100100,
-        0b00100100,
-        0b00111100,
-        0b00100100,
-        0b00100100,
-        0b00000000,
-        
-        0b00000000,
-        0b00100100,
-        0b00100100,
-        0b00111100,
-        0b00100100,
-        0b00100100,
-        0b00000000,
-        };
+        0b00000000};
 int main()
 {
     TCCR1A = 0b00000000; // CTC
@@ -72,26 +84,106 @@ int main()
     set_bit(DDRB, PB1); // led 8
     set_bit(DDRB, PB2); // led 8
 
-    int8_t indice = 1;
     while (1)
     {
-        if (indice < 8)
+        switch (matris)
         {
-            wels_col((uint64_t)(hola[indice])<<32);
-            wels_fila( (1<<indice) );
+        case fila1:
+            for (size_t i = 0; i < 40; i++)
+            {
+                beats_ini = 0b00000001 & (hola[i]);
+                beats = beats | (beats_ini << i);
+            }
+            wels_col(beats);
+            wels_fila(mux_vec[1]);
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB1);
+            beats = beats_ini = 0;
+            matris = fila2;
+            break;
+        case fila2:
+            for (size_t i = 0; i < 40; i++)
+            {
+                beats_ini = 1 >> (0b00000010 & (hola[i]));
+                beats = beats | (beats_ini << i);
+            }
+            wels_col(~beats);
+            wels_fila(mux_vec[2]);
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB1);
+            beats = beats_ini = 0;
+            matris = fila3;
+            break;
+        case fila3:
+            for (size_t i = 0; i < 40; i++)
+            {
+                beats_ini = (hola[i] >> 2) & 1;
+                beats = beats | (beats_ini << i);
+            }
+            wels_col(beats);
+            wels_fila(mux_vec[3]);
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB1);
 
-            set_bit(PORTB, latch_pin);
-            clear_bit(PORTB, latch_pin);
-            indice++;
-        }
-        else
-        {
-            indice=1;
-            wels_col(hola[indice]);
-            wels_fila(mux_vec[indice]);
+            beats = beats_ini = 0;
+            matris = fila4;
+            break;
+        case fila4:
+            for (size_t i = 0; i < 40; i++)
+            {
+                beats_ini = (hola[i] >> 3) & 1;
+                beats = beats | (beats_ini << i);
+            }
+            wels_col(beats);
+            wels_fila(mux_vec[4]);
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB1);
 
-            set_bit(PORTB, latch_pin);
-            clear_bit(PORTB, latch_pin);
+            beats = beats_ini = 0;
+            matris = fila5;
+            break;
+        case fila5:
+            for (size_t i = 0; i < 40; i++)
+            {
+                beats_ini = (hola[i] >> 4) & 1;
+                beats = beats | (beats_ini << i);
+            }
+            wels_col(beats);
+            wels_fila(mux_vec[5]);
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB1);
+
+            beats = beats_ini = 0;
+            matris = fila6;
+            break;
+        case fila6:
+            for (size_t i = 0; i < 40; i++)
+            {
+                beats_ini = (hola[i] >> 5) & 1;
+                beats = beats | (beats_ini << i);
+            }
+            wels_col(beats);
+            wels_fila(mux_vec[6]);
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB1);
+
+            beats = beats_ini = 0;
+            matris = fila7;
+            break;
+        case fila7:
+            for (size_t i = 0; i < 40; i++)
+            {
+                beats_ini = (hola[i] >> 6) & 1;
+                beats = beats | (beats_ini << i);
+            }
+            wels_col(beats);
+            wels_fila(mux_vec[7]);
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB1);
+
+            beats = beats_ini = 0;
+            matris = fila1;
+            break;
         }
     }
 }
