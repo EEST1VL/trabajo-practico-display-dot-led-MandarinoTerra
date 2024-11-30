dd3a2de6c712748866dbcefc6ea3a50f3cdf3d32
MandarinoTerra <benjaminjunco383@gmail.com>
2024-11-30 13:20:04 -0300

    Actividad1y2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,37 @@
 #include <Arduino.h>
+#include "encoder.c"
+#define set_bit(reg, bit) reg |= (1 << bit)
+#define clear_bit(reg, bit) reg &= ~(1 << bit)
 
-int main(void){
-  while (1)
-  
-  {
-  asm(nop);  /* code */
-  }
-  
+int8_t angle = 10;
+ISR(INT0_vect) // me interrumpio el PD2
+{
+    (PIND & 0b00001000) ? angle-- : angle++; // le pregunto el estado al PD3
+}
+
+ISR(INT1_vect) // me interrumpio el PD3
+{
+    (PIND & 0b00000100) ? angle++ : angle--; // le pregunto el estado al PD2
+}
+int main(void)
+{
+    initInterrupts();
+    sei();
+    set_bit(DDRB, PB0);
+    set_bit(DDRB, PB1);
+    while (1)
+    {
+        if (angle > 10)
+        {
+            set_bit(PORTB, PB0);
+            angle = 10;
+            clear_bit(PORTB, PB1);
+        }
+        if (angle < 10)
+        {
+            set_bit(PORTB, PB1);
+            clear_bit(PORTB, PB0);
+            angle = 10;
+        }
+    }
 }
